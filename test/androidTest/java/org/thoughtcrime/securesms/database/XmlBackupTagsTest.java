075818eb50c8c703a8fb345f4b26d4baa3568f50
merkste <merkste@web.de>
2016-02-21 18:58:59 +0100

    Document attribute names of XML backup

---
+++ test/androidTest/java/org/thoughtcrime/securesms/database/XmlBackupTagsTest.java
@@ -0,0 +1,86 @@
+package org.thoughtcrime.securesms.database;
+
+
+import junit.framework.TestCase;
+
+import android.provider.Telephony.BaseMmsColumns;
+import android.provider.Telephony.Mms;
+import android.provider.Telephony.TextBasedSmsColumns;
+
+/**
+ * Document XML backup attributes corresponding to Android database column names.
+ * Breaks if the names change in a future Android version.
+ */
+public class XmlBackupTagsTest extends TestCase {
+
+    public void testCommonAttributes() {    // common attributes names
+        assertEquals(TextBasedSmsColumns.ADDRESS,   "address");   // text
+        assertEquals(TextBasedSmsColumns.DATE,      "date");      // int
+        assertEquals(TextBasedSmsColumns.DATE_SENT, "date_sent"); // int
+        assertEquals(TextBasedSmsColumns.READ,      "read");      // int (boolean)
+        assertEquals(TextBasedSmsColumns.LOCKED,    "locked");    // int (boolean)
+        final String READABLE_DATE   = "readable_date";           // text, SMS Backup & Restore
+        final String CONTACT_NAME    = "contact_name";            // text, SMS Backup & Restore
+        final String THREAD_ADDRESS  = "thread_address";          // text, Signal
+    }
+
+    public void testSmsAttributes() {
+        assertEquals(TextBasedSmsColumns.STATUS,         "status");         // int
+        assertEquals(TextBasedSmsColumns.PROTOCOL,       "protocol");       // int
+        assertEquals(TextBasedSmsColumns.TYPE,           "type");           // int
+        assertEquals(TextBasedSmsColumns.SUBJECT,        "subject");        // text
+        assertEquals(TextBasedSmsColumns.BODY,           "body");           // text
+        assertEquals(TextBasedSmsColumns.SERVICE_CENTER, "service_center"); // text
+        final String TOA    = "toa";                                        // text, SMS Backup & Restore
+        final String SC_TOA = "sc_toa";                                     // text, SMS Backup & Restore
+    }
+
+    public void testMmsAttributes() {
+        assertEquals(BaseMmsColumns.TEXT_ONLY,             "text_only");   // int (boolean)
+        assertEquals(BaseMmsColumns.SUBJECT,               "sub");         // text
+        assertEquals(BaseMmsColumns.RETRIEVE_STATUS,       "retr_st");     // int
+        assertEquals(BaseMmsColumns.CONTENT_CLASS,         "ct_cls");      // int
+        assertEquals(BaseMmsColumns.SUBJECT_CHARSET,       "sub_cs");      // subject
+        assertEquals(BaseMmsColumns.CONTENT_LOCATION,      "ct_l");        // text
+        assertEquals(BaseMmsColumns.TRANSACTION_ID,        "tr_id");       // id
+        assertEquals(BaseMmsColumns.STATUS,                "st");          // int
+        assertEquals(BaseMmsColumns.MESSAGE_BOX,            "msg_box");    // int, e.g., MESSAGE_BOX_INBOX
+        assertEquals(BaseMmsColumns.MESSAGE_CLASS,         "m_cls");       // text
+        assertEquals(BaseMmsColumns.DELIVERY_TIME,         "d_tm");        // int
+        assertEquals(BaseMmsColumns.READ_STATUS,           "read_status"); // int
+        assertEquals(BaseMmsColumns.CONTENT_TYPE,          "ct_t");        // text
+        assertEquals(BaseMmsColumns.RETRIEVE_TEXT_CHARSET, "retr_txt_cs"); // int
+        assertEquals(BaseMmsColumns.DELIVERY_REPORT,       "d_rpt");       // int
+        assertEquals(BaseMmsColumns.MESSAGE_ID,            "m_id");        // text
+        assertEquals(BaseMmsColumns.SEEN,                  "seen");        // int (boolean)
+        assertEquals(BaseMmsColumns.MESSAGE_TYPE,          "m_type");      // int
+        assertEquals(BaseMmsColumns.MMS_VERSION,           "v");           // int
+        assertEquals(BaseMmsColumns.EXPIRY,                "exp");         // int
+        assertEquals(BaseMmsColumns.PRIORITY,              "pri");         // int
+        assertEquals(BaseMmsColumns.READ_REPORT,           "rr");          // int (boolean)
+        assertEquals(BaseMmsColumns.RESPONSE_TEXT,         "resp_txt");    // text
+        assertEquals(BaseMmsColumns.REPORT_ALLOWED,        "rpt_a");       // int (boolean)
+        assertEquals(BaseMmsColumns.SUBSCRIPTION_ID,       "sub_id");      // int (long), < 0 if unknown
+        assertEquals(BaseMmsColumns.RETRIEVE_TEXT,         "retr_txt");    // text
+        assertEquals(BaseMmsColumns.RESPONSE_STATUS,       "resp_st");     // int
+        assertEquals(BaseMmsColumns.MESSAGE_SIZE,          "m_size");      // int
+        assertEquals(BaseMmsColumns.CREATOR,               "creator");     // text, e.g., app name
+    }
+
+    public void testPartAttributes() {
+        assertEquals(Mms.Part.SEQ,                 "seq");   // int
+        assertEquals(Mms.Part.CONTENT_TYPE,        "ct");    // content type, text (MIME ?)
+        assertEquals(Mms.Part.NAME,                "name");  // name, text
+        assertEquals(Mms.Part.CHARSET,             "chset"); // charset, text
+        assertEquals(Mms.Part.CONTENT_DISPOSITION, "cd");    // content disposition, text
+        assertEquals(Mms.Part.FILENAME,            "fn");    // filename, text
+        assertEquals(Mms.Part.CONTENT_ID,          "cid");   // content id, int
+        assertEquals(Mms.Part.CONTENT_LOCATION,    "cl");    // content location, int
+        assertEquals(Mms.Part.CT_START,            "ctt_s"); // start of content type, int
+        assertEquals(Mms.Part.CT_TYPE,             "ctt_t"); // type of content type, text
+        assertEquals(Mms.Part.TEXT,                "text");  // text
+        final String DISPLAY_NAME = "_display_name";         // text,   SMS Backup & Restore
+        final String SIZE         = "_size";                 // int,    SMS Backup & Restore
+        final String DATA         = "data";                  // base64, SMS Backup & Restore
+    }
+}
